6671d6e55b384bc3e8079137be7cc4abc3393646
Justin Dietert <justin.dietert@gmail.com>
2015-04-28 09:59:42 -0500

    renamted portfolio blade to 'work'

---
+++ app/views/work.blade.php
@@ -0,0 +1,32 @@
+@extends('layouts.master')
+
+@section('title')
+    <title>Justin Dietert | Work</title>
+@stop
+
+@section('description')
+    <meta name="description" content="Justin Dietert's portfolio">
+@stop
+
+@section('nav-active')
+    <li><a href="/">Home</a></li>
+    <li class="active"><a href="/work">Work</a></li>
+    <li><a href="/about">About</a></li>
+    <li><a href="/resume">Resum&eacute;</a></li>
+    <li><a href="/blog">Blog</a></li>
+@stop
+
+@section('content')
+        <div role="main" class="main">
+            <div class="inner">
+
+                <h2>Work</h2>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quod porro perspiciatis nesciunt repudiandae error, provident, facere quasi fugit nisi quis hic facilis pariatur aliquam inventore.</p>
+                <h3>Clean design</h3>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque beatae pariatur laudantium incidunt fugit facilis, eligendi, delectus voluptate omnis veritatis reprehenderit. Ducimus, cupiditate. Reiciendis, voluptas!</p>
+                <h4>Clean design</h4>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ullam quis nostrum quo optio, officia ex cum sint est ipsam. Possimus inventore ab minus voluptas fugit!</p>
+     
+            </div>
+        </div>
+@stop
