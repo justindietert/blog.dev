36b8949e9f40bf0cb61421f47cdde87c32ef92d7
Justin Dietert <justin.dietert@gmail.com>
2015-04-29 13:19:37 -0500

    updated text on work thumbnails for testing

--- app/views/work.blade.php
+++ app/views/work.blade.php
@@ -21,35 +21,35 @@
             <div class="inner">
                 <div class="row">
                     <div class="col one">
-                        <a href="#" class="thumb"><img src="http://placehold.it/400x400" alt=""><span>Text Here</span></a>
+                        <a href="#" class="thumb"><img src="http://placehold.it/400x400" alt=""><span>Item 1</span></a>
                     </div>
                     <div class="col two">
-                        <a href="#" class="thumb"><img src="http://placehold.it/400x400" alt=""><span>Text Here</span></a>
+                        <a href="#" class="thumb"><img src="http://placehold.it/400x400" alt=""><span>Item 2</span></a>
                     </div>
                     <div class="col three">
-                        <a href="#" class="thumb"><img src="http://placehold.it/400x400" alt=""><span>Text Here</span></a>
+                        <a href="#" class="thumb"><img src="http://placehold.it/400x400" alt=""><span>Item 3</span></a>
                     </div>
                 </div>
                 <div class="row">
                     <div class="col one">
-                        <a href="#" class="thumb"><img src="http://placehold.it/400x400" alt=""><span>Text Here</span></a>
+                        <a href="#" class="thumb"><img src="http://placehold.it/400x400" alt=""><span>Item 4</span></a>
                     </div>
                     <div class="col two">
-                        <a href="#" class="thumb"><img src="http://placehold.it/400x400" alt=""><span>Text Here</span></a>
+                        <a href="#" class="thumb"><img src="http://placehold.it/400x400" alt=""><span>Item 5</span></a>
                     </div>
                     <div class="col three">
-                        <a href="#" class="thumb"><img src="http://placehold.it/400x400" alt=""><span>Text Here</span></a>
+                        <a href="#" class="thumb"><img src="http://placehold.it/400x400" alt=""><span>Item 6</span></a>
                     </div>
                 </div>
                 <div class="row">
                     <div class="col one">
-                        <a href="#" class="thumb"><img src="http://placehold.it/400x400" alt=""><span>Text Here</span></a>
+                        <a href="#" class="thumb"><img src="http://placehold.it/400x400" alt=""><span>Item 7</span></a>
                     </div>
                     <div class="col two">
-                        <a href="#" class="thumb"><img src="http://placehold.it/400x400" alt=""><span>Text Here</span></a>
+                        <a href="#" class="thumb"><img src="http://placehold.it/400x400" alt=""><span>Item 8</span></a>
                     </div>
                     <div class="col three">
-                        <a href="#" class="thumb"><img src="http://placehold.it/400x400" alt=""><span>Text Here</span></a>
+                        <a href="#" class="thumb"><img src="http://placehold.it/400x400" alt=""><span>Item 9</span></a>
                     </div>
                 </div>
             </div>
