50c8b298a2cf51e77a2554d34db6742621efe635
Justin Dietert <justin.dietert@gmail.com>
2015-04-28 21:23:06 -0500

    implementing grid for portfolio thumbnails

--- app/views/work.blade.php
+++ app/views/work.blade.php
@@ -19,12 +19,39 @@
 @section('content')
         <div role="main" class="main">
             <div class="inner">
-
-                <h2>Work</h2>
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quod porro perspiciatis nesciunt repudiandae error, provident, facere quasi fugit nisi quis hic facilis pariatur aliquam inventore.</p>
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quidem tempore est maxime velit? Laboriosam praesentium minima id officiis ullam non mollitia incidunt excepturi, quas at laudantium sequi maxime sint, cupiditate itaque iusto in doloribus quo veritatis vero. Laborum dolorum repellat officia nobis perferendis est, dignissimos quasi quas maiores recusandae? Iste!</p>
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam, perferendis neque consequatur esse consequuntur odio sunt cumque. Maiores magnam, distinctio similique labore inventore fugit obcaecati, necessitatibus ratione cupiditate? Tempore animi, corrupti ducimus saepe possimus commodi assumenda praesentium nesciunt aliquam sit eos. Odio reprehenderit quaerat in asperiores praesentium cupiditate, soluta ea odit dicta mollitia iusto possimus alias iste optio ad, labore omnis quis suscipit dolorem quibusdam et sunt doloremque? Nihil non voluptates, voluptatum sunt laboriosam numquam quibusdam delectus aliquam eius doloremque, eligendi alias autem placeat nemo aperiam! Est itaque quasi voluptatum expedita officiis, sequi velit iste deserunt mollitia fugit, cumque voluptatem?</p>
-     
+                <div class="row">
+                    <div class="col one">
+                        <img src="http://placehold.it/400x400" alt="">
+                    </div>
+                    <div class="col two">
+                        <img src="http://placehold.it/400x400" alt="">
+                    </div>
+                    <div class="col three">
+                        <img src="http://placehold.it/400x400" alt="">
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col one">
+                        <img src="http://placehold.it/400x400" alt="">
+                    </div>
+                    <div class="col two">
+                        <img src="http://placehold.it/400x400" alt="">
+                    </div>
+                    <div class="col three">
+                        <img src="http://placehold.it/400x400" alt="">
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col one">
+                        <img src="http://placehold.it/400x400" alt="">
+                    </div>
+                    <div class="col two">
+                        <img src="http://placehold.it/400x400" alt="">
+                    </div>
+                    <div class="col three">
+                        <img src="http://placehold.it/400x400" alt="">
+                    </div>
+                </div>
             </div>
         </div>
 @stop
