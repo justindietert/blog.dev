390c6d85e9b0c2eaa95db76d063f222feda83197
Justin Dietert <justin.dietert@gmail.com>
2015-05-05 14:09:53 -0500

    fixed seeder problem by forcing Faker to generate only unique usernames and emails

--- app/database/seeds/PostsTableSeeder.php
+++ app/database/seeds/PostsTableSeeder.php
@@ -18,21 +18,16 @@ class PostsTableSeeder extends Seeder
 
         for ($i = 1; $i <= 100; $i++) {
             $rand_body  = rand(50, 300);
-            $post = Post::create([
-                'title' => $faker->text(20),
-                'slug' => $faker->text(20),
-                'body' => $faker->sentence($rand_body),
-                'user_id' => rand(1, 100)
-            ]);
+            $rand_title = rand(2, 4);
+
+            $post = new Post();
+            $post->title = $faker->sentence($rand_title);
+            $post->slug = $post->title;
+            $post->body = $faker->sentence($rand_body);
+            $post->user_id = rand(1, 100);
+            $post->save();
         }
 
-        // for ($i = 1; $i <= 10; $i++) {
-        //     $post = new Post();
-        //     $post->title = "Title $i";
-        //     $post->slug = $post->title;
-        //     $post->body = "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Facere, consectetur, cupiditate dolorum hic expedita nam minus illo voluptates placeat veritatis numquam, dolor facilis? Ipsam sit maxime temporibus deleniti ipsum, dolorem, sapiente enim aspernatur distinctio esse laborum porro debitis voluptates soluta tenetur. Fugiat commodi asperiores et harum consequatur dignissimos sed at!";
-        //     $post->user_id = 1;
-        //     $post->save();
-        // }
     }
+
 }
--- app/database/seeds/UsersTableSeeder.php
+++ app/database/seeds/UsersTableSeeder.php
@@ -17,23 +17,19 @@ class UsersTableSeeder extends Seeder
         $faker = Factory::create();
 
         for ($i = 1; $i <= 100; $i++) {
-            $user = User::create([
-                'username' => $faker->userName,
-                'email' => $faker->email,
-                'password' => $faker->password
-            ]);
+            $user = new User();
+            $user->username = $faker->unique()->userName;
+            $user->email    = $faker->unique()->email;
+            $user->password = $faker->password;
+            $user->save();
         }
 
-        // $user1 = new User();
-        // $user1->username = 'justind';
-        // $user1->email    = 'justin.dietert@gmail.com';
-        // $user1->password = $_ENV['USER_PASS'];
-        // $user1->save();
+        $user1 = new User();
+        $user1->username = 'justind';
+        $user1->email    = 'justin.dietert@gmail.com';
+        $user1->password = $_ENV['USER_PASS'];
+        $user1->save();
 
-        // $user2 = new User();
-        // $user2->username = 'kenny';
-        // $user2->email    = 'kp@email.com';
-        // $user2->password = $_ENV['USER_PASS'];
-        // $user2->save();
     }
+
 }
