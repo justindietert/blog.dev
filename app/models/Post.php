394807859fddf4bebef76d377f1e1134465c9e61
Justin Dietert <justin.dietert@gmail.com>
2015-05-07 12:15:59 -0500

    changed slug mutator to generate unique slugs if it already exists in the DB

--- app/models/Post.php
+++ app/models/Post.php
@@ -10,11 +10,17 @@ class Post extends BaseModel
     );
 
     /**
-     * Format slug from title.
+     * Format slug from title and make it unique if it already exists in DB.
      */
     public function setSlugAttribute($value)
     {
-        $this->attributes['slug'] = Str::slug($value);
+        $slug = Str::slug($value);
+
+        $slugCount = count( $this->whereRaw("slug REGEXP '^{$slug}(-[0-9]*)?$'")->get() );
+
+        $slugFinal = ($slugCount > 0) ? "{$slug}-{$slugCount}" : $slug;
+
+        $this->attributes['slug'] = $slugFinal;
     }
 
     /**
