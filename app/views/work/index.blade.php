0f79addd86c652051b5f153d873eab6bcfb253a4
Justin Dietert <justin.dietert@gmail.com>
2015-05-08 23:17:31 -0500

    overhaul of work page grid and style updates

--- app/views/work/index.blade.php
+++ app/views/work/index.blade.php
@@ -18,53 +18,59 @@
 
 @section('content')
         <div role="main" class="main" id="work">
-            <div class="inner">
+            <div class="inner full">
                 <div class="row">
                     <div class="col one">
-                        <a href="{{{ action('WorkController@space') }}}" class="thumb"><img src="/img/work/thumbs/space_thumb.jpg" alt="Partners in Space"><span>Partners in Space</span></a>
+                        <a href="{{{ action('WorkController@space') }}}" class="thumb">
+                            <img src="/img/work/thumbs/space_thumb.jpg" alt="Partners in Space">
+                            <div class="title-overlay">
+                                <h2>Partners in Space</h2>
+                                <p>print design</p>
+                            </div>
+                        </a>
                     </div>
                     <div class="col two">
-                        <a href="{{{ action('WorkController@whackamole') }}}" class="thumb"><img src="/img/work/thumbs/whack_thumb.jpg" alt="Whack-a-Mole!"><span>Whack-a-Mole!</span></a>
+                        <a href="{{{ action('WorkController@whackamole') }}}" class="thumb"><img src="/img/work/thumbs/whack_thumb.jpg" alt="Whack-a-Mole!"></a>
                     </div>
                     <div class="col three">
-                        <a href="#" class="thumb"><img src="/img/work/thumbs/shepard_thumb.jpg" alt="Sam Shepard"><span>Sam Shepard</span></a>
+                        <a href="#" class="thumb"><img src="/img/work/thumbs/shepard_thumb.jpg" alt="Sam Shepard"></a>
                     </div>
 
                     <div class="col one">
-                        <a href="#" class="thumb"><img src="/img/work/thumbs/salute_thumb.jpg" alt="Salute to Freedom"><span>Salute to Freedom</span></a>
+                        <a href="#" class="thumb"><img src="/img/work/thumbs/salute_thumb.jpg" alt="Salute to Freedom"></a>
                     </div>
                     <div class="col two">
-                        <a href="#" class="thumb"><img src="/img/work/thumbs/stem_thumb.jpg" alt="STEM Graduates"><span>STEM Graduates</span></a>
+                        <a href="#" class="thumb"><img src="/img/work/thumbs/stem_thumb.jpg" alt="STEM Graduates"></a>
                     </div>
                     <div class="col three">
-                        <a href="{{{ action('WorkController@simpleSimon') }}}" class="thumb"><img src="/img/work/thumbs/simon_thumb.jpg" alt="Simple Simon"><span>Simple Simon</span></a>
+                        <a href="{{{ action('WorkController@simpleSimon') }}}" class="thumb"><img src="/img/work/thumbs/simon_thumb.jpg" alt="Simple Simon"></a>
                     </div>
 
                     <div class="col one">
-                        <a href="#" class="thumb"><img src="/img/work/thumbs/hillviews_thumb.jpg" alt="Hillviews Cover"><span><em>Hillviews</em> Cover</span></a>
+                        <a href="#" class="thumb"><img src="/img/work/thumbs/hillviews_thumb.jpg" alt="Hillviews Cover"></a>
                     </div>
                     <div class="col two">
-                        <a href="#" class="thumb"><img src="/img/work/thumbs/social_thumb.jpg" alt="TXST Social Media"><span>TXST Social Media</span></a>
+                        <a href="#" class="thumb"><img src="/img/work/thumbs/social_thumb.jpg" alt="TXST Social Media"></a>
                     </div>
                     <div class="col three">
-                        <a href="#" class="thumb"><img src="/img/work/thumbs/firestation_thumb.jpg" alt="Fire Station Studios"><span>Fire Station Studios</span></a>
+                        <a href="#" class="thumb"><img src="/img/work/thumbs/firestation_thumb.jpg" alt="Fire Station Studios"></a>
                     </div>
 
                     <div class="col one">
-                        <a href="#" class="thumb"><img src="/img/work/thumbs/takeaseat_thumb.jpg" alt="Take a Seat"><span>Take a Seat</span></a>
+                        <a href="#" class="thumb"><img src="/img/work/thumbs/takeaseat_thumb.jpg" alt="Take a Seat"></a>
                     </div>
                     <div class="col two">
-                        <a href="#" class="thumb"><img src="/img/work/thumbs/logos_thumb.jpg" alt="Various Identities"><span>Various Identities</span></a>
+                        <a href="#" class="thumb"><img src="/img/work/thumbs/logos_thumb.jpg" alt="Various Identities"></a>
                     </div>
                     <div class="col three">
-                        <a href="#" class="thumb"><img src="/img/work/thumbs/retention_thumb.jpg" alt="TXST Retention"><span>TXST Retention</span></a>
+                        <a href="#" class="thumb"><img src="/img/work/thumbs/retention_thumb.jpg" alt="TXST Retention"></a>
                     </div>
 
                     <div class="col one">
-                        <a href="#" class="thumb"><img src="/img/work/thumbs/foster_thumb.jpg" alt="Jeff Foster Invite"><span>Jeff Foster Invite</span></a>
+                        <a href="#" class="thumb"><img src="/img/work/thumbs/foster_thumb.jpg" alt="Jeff Foster Invite"></a>
                     </div>
                     <div class="col two">
-                        <a href="#" class="thumb"><img src="/img/work/thumbs/whysociology_thumb.jpg" alt="Why Sociology?"><span>Why Sociology?</span></a>
+                        <a href="#" class="thumb"><img src="/img/work/thumbs/whysociology_thumb.jpg" alt="Why Sociology?"></a>
                     </div>
 
                 </div>
