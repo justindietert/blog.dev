5b0d4b86ef835dfa8c7d43e245b06ad6071b3211
Justin Dietert <justin.dietert@gmail.com>
2015-05-30 18:17:14 -0500

    updates to work show structure and added assets

--- app/views/work/partners-in-space.blade.php
+++ app/views/work/partners-in-space.blade.php
@@ -17,10 +17,40 @@
 @section('content')
         <div role="main" class="main" id="work-show">
             <div class="inner full">
-                <img src="/img/work/spreads/space_spread.jpg" alt="Partners in Space magazine spread">
-            </div>
-            <div class="inner full">
-                <h2>Partners in Space</h2>
+                <div class="row">
+                    <div class="col one-third">
+                        <h2>Partners in Space</h2>
+                        <hr class="work-divider">
+                        <h3>Client</h3>
+                        <ul>
+                            <li>Texas State University</li>
+                        </ul>
+                        <h3>Role</h3>
+                        <ul>
+                            <li>print design</li>
+                        </ul>
+                    </div>
+                    <div class="col two-thirds">
+                        <p class="work-intro">
+                            Designed for Texas State University's alumni magazine, <em>Hillviews</em>, the Partners in Space article needed to highlight the collaboration between Texas State, Jacobs Engineering, and NASA on projects such as the International Space Station and the manned mission to Mars.
+                        </p>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col full-spread">
+                        <img src="/img/work/spreads/space_spread_hero-2.jpg" alt="">
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col full-spread">
+                        <img src="/img/work/spreads/space_spread_zoom-mars-2.jpg" alt="">
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col full-spread">
+                        <img src="/img/work/spreads/space_spread.jpg" alt="Partners in Space magazine spread">
+                    </div>
+                </div>
             </div>
         </div>
 @stop
