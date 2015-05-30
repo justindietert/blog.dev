66eff98c1ce89b82c8de5a23cd496557ea557650
Justin Dietert <justin.dietert@gmail.com>
2015-05-29 21:43:03 -0500

    updates to resume content

--- app/views/resume.blade.php
+++ app/views/resume.blade.php
@@ -23,13 +23,17 @@
 
                 <div class="row">
                     <div class="col sixty">
+                        <h2 class="typl8-epsilon">Objective</h2>
+                        <p>To combine my 10 years of graphic design experience with my full-stack programming skills to add value to a company that demands only the best from its employees and values mentorship and continued learning.</p>
+
                         <h2 class="typl8-epsilon">Experience</h2>
-                        <h3 class="typl8-delta">Freelance Designer and&nbsp;Web&nbsp;Developer</h3>
-                        <h6>May 2015 &ndash; present | San Antonio, Texas</h6>
+
+                        <h3 class="typl8-delta">Codeup Bootcamp Projects</h3>
+                        <h6>April &ndash; May 2015 | San Antonio, Texas</h6>
                         <ul class="experience">
-                            <li>Designing and building responsive websites and applications with a focus on engaging user experiences and clean user interfaces</li>
-                            <li>Development done in a Vagrant environment utilizing the Laravel <abbr title="Model, View, Controller">MVC</abbr> framework and the <abbr title="Linux, Apache, MySQL, PHP + JavaScript">LAMP+J</abbr> stack</li>
-                            <li>Version control with Git and GitHub</li>
+                            <li><em>Adlister</em>: a Craigslist clone created in <abbr>php</abbr> over the course of one week with a partner. My main contributions were designing and building a customized front-end with the Zurb Foundation framework and integrating it with the backend functionality.</li>
+                            <li><em>Blog (justindietert.com)</em>: a solo two-week portfolio and online resum&eacute; project, as well as a personal blog. Built with Laravel, utilizing the full <abbr title="Linux, Nginx, MySQL, PHP + JavaScript">lemp+j</abbr> stack. I also used <abbr>sass</abbr> and implemented a custom responsive grid system, as well as custom JavaScript/jQuery components.</li>
+                            <li><em>TyleNinja</em>: a two-week team capstone project. TyleNinja is a tile-sliding puzzle game web app, built from scratch using Laravel, JavaScript, jQuery, and MySQL. The full application can be viewed <a href="http://tyle.ninja/" target="_blank">here</a>.</li>
                         </ul>
 
                         <h3 class="typl8-delta">Senior Graphic Designer</h3>
@@ -37,9 +41,8 @@
                         <h6>July 2012 &ndash; December 2014 | San Marcos,&nbsp;Texas</h6>
                         <ul class="experience">
                             <li>Worked with clients from many of the diverse departments and offices of the university to effectively market their programs and services to both internal and external audiences</li>
-                            <li>Designed within the university’s branding guidelines, managing multiple projects simultaneously from conception to completion</li>
+                            <li>Designed within the university’s branding guidelines, managing multiple projects simultaneously from conception to completion while collaborating with a team of writers, photographers, and videographers</li>
                             <li>Projects included publication design, magazine ad design, invitation design, brochure design, logo design, booklet design, email marketing, and other printed materials</li>
-                            <li>Collaborated with in-house team of graphic designers, writers, photographers, and videographers on a daily basis</li>
                         </ul>
 
                         <h3 class="typl8-delta">Marketing and Design Manager</h3>
@@ -48,18 +51,14 @@
                         <ul class="experience">
                             <li>Increased sales and revenue by marketing company services to professional clients and supervised design staff to ensure success of projects</li>
                             <li>Lead designer for print and online advertising campaigns and in charge of monthly email newsletters from concept to <abbr>HTML</abbr> code</li>
-                            <li>Assisted with vendor and product selection, product development, and regularly attended trade shows to make services recommendations</li>
-                            <li>Planned special events and delivered presentations to professional clients</li>
                         </ul>
 
                         <h3 class="typl8-delta">Graphic Designer</h3>
                         <h6>Digital Pro Lab</h6>
                         <h6>2005 &ndash; 2010 | San Antonio, Texas</h6>
                         <ul class="experience">
-                            <li>Worked with in-house design team to create digital and print marketing material for company and professional clients</li>
+                            <li>Worked with in-house design team to create digital and print marketing material for company and professional clients while managing multiple projects and meeting tight deadlines</li>
                             <li>Supervised company website re-design while providing support to in-house web designer through the creation of graphics and written content</li>
-                            <li>Responsible for managing multiple projects simultaneously and meeting tight deadlines required by professional clients</li>
-                            <li>In charge of product photography</li>
                         </ul>
                     </div>
                     <div class="col forty">
@@ -67,7 +66,7 @@
                         <h3 class="typl8-delta">Codeup Bootcamp</h3>
                         <h6>Graduated May 2015</h6>
                         <ul class="education">
-                            <li><abbr title="Linux, Apache, MySQL, PHP + JavaScript">LAMP+J</abbr> Web Development</li>
+                            <li><abbr title="Linux, Nginx, MySQL, PHP + JavaScript">LEMP+J</abbr> Web Development</li>
                             <li>4-month immersive bootcamp</li>
                         </ul>
                         <h3 class="typl8-delta">Bachelor of Fine Arts, New Media</h3>
@@ -79,9 +78,9 @@
 
                         <h2 class="typl8-epsilon">Skills</h2>
                         <ul>
-                            <li><abbr>HTML5</abbr>, <abbr>CSS3</abbr>, <abbr title="Linux, Apache, MySQL, PHP">LAMP</abbr> stack, JavaScript, jQuery</li>
+                            <li><abbr>HTML5</abbr>, <abbr>CSS3</abbr>, <abbr title="Linux, Nginx, MySQL, PHP">LEMP</abbr> stack, JavaScript, jQuery</li>
                             <li>Vagrant, Laravel <abbr title="Model, View, Controller">MVC</abbr> framework, Sequel Pro</li>
-                            <li>Zurb Foundation and Twitter Bootstrap</li>
+                            <li>Zurb Foundation, Twitter Bootstrap, Materialize <abbr>CSS</abbr></li>
                             <li>Version control &mdash; Git and GitHub</li>
                             <li>Brand and visual identity creation</li>
                             <li>Layout, typography, color theory, photography</li>
